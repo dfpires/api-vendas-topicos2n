183b8b7f9cac4933b589c2791dd57f2875cc5856
Daniel Pires <dfpires@gmail.com>
2021-11-06 08:28:29 -0300

    06-11

--- src/modules/products/controllers/ProductController.ts
+++ src/modules/products/controllers/ProductController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import CreateProductService from "../services/CreateProductService";
+import DeleteProductService from "../services/DeleteProductService";
 import ListProductService from "../services/ListProductService";
 import ShowProductService from "../services/ShowProductService";
+import UpdateProductService from "../services/UpdateProductService";
 
 // observem que não há regra de negócio aqui, elas estão nos services
 export default class ProductController {
@@ -27,5 +29,22 @@ export default class ProductController {
         const product = await createProduct.execute({name, quantity, price})
         return response.json(product)
     }
-    
+    // vai chamar o UpdateProductService
+    public async update(request: Request, response: Response): Promise<Response> {
+        // recupera o id do produto que vem dentro dos parâmetros da URL
+        const {id} = request.params
+         // recupera os dados do produto que vem no corpo (body) do requisição
+         const {name, quantity, price} = request.body
+         const updateProduct = new UpdateProductService()
+         const product = await updateProduct.execute({id, name, quantity, price})
+        return response.json(product)
+    }
+    // vai chamar o DeleteProductService
+    public async delete(request: Request, response: Response): Promise<Response> {
+         // recupera o id do produto que vem dentro dos parâmetros da URL
+         const {id} = request.params
+         const deleteProduct = new DeleteProductService()
+         await deleteProduct.execute(id)
+        return response.json([])
+    }
 }
--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -4,7 +4,7 @@ import ProductRepository from "../typeorm/repositories/ProductRepository";
 
 class DeleteProductService {
 
-    public async execute(id: number): Promise<void> {
+    public async execute(id: string): Promise<void> {
 
         // não podemos remover um produto que não exista
         const productRepository = getCustomRepository(ProductRepository)
