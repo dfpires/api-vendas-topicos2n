2333722d586209b170ce8c69f5582a1cbfa986b2
Daniel Pires <dfpires@gmail.com>
2021-11-05 22:03:15 -0300

    05-11

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,14 @@
+import { getCustomRepository } from "typeorm";
+import Product from "../typeorm/entities/Product";
+import ProductRepository from "../typeorm/repositories/ProductRepository";
+
+class ListProductService {
+
+    public async execute(): Promise<Product[]>{
+
+        const productRepository = getCustomRepository(ProductRepository)
+        const products = await productRepository.find()
+        return products
+
+    }
+}
