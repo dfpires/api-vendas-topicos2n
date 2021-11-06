54abd236e94bbc76d6e76bbb4c51df02d2af42c3
Daniel Pires <dfpires@gmail.com>
2021-11-06 08:12:18 -0300

    06-11

--- src/modules/products/controllers/ProductController.ts
+++ src/modules/products/controllers/ProductController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import CreateProductService from "../services/CreateProductService";
 import ListProductService from "../services/ListProductService";
+import ShowProductService from "../services/ShowProductService";
 
 // observem que não há regra de negócio aqui, elas estão nos services
 export default class ProductController {
@@ -9,4 +11,21 @@ export default class ProductController {
         const products = await listProduct.execute()
         return response.json(products) // converte para JSON e retorna
     }
+    // vai chamar o ShowProductService
+    public async show(request: Request, response: Response): Promise<Response> {
+        // recupera o id do produto que vem dentro dos parâmetros da URL
+        const {id} = request.params
+        const showProduct = new ShowProductService()
+        const product = await showProduct.execute(id)
+        return response.json(product)
+    }
+    // vai chamar o CreateProductService
+    public async create(request: Request, response: Response): Promise<Response> {
+        // recupera os dados do produto que vem no corpo (body) do requisição
+        const {name, quantity, price} = request.body
+        const createProduct = new CreateProductService()
+        const product = await createProduct.execute({name, quantity, price})
+        return response.json(product)
+    }
+    
 }
