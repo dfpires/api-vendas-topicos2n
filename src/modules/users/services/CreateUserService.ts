13ea002479ed83be0ac90ae7b05d0e42b4c050c6
dfpires@gmail.com <dfpires@gmail.com>
2021-11-18 20:11:14 -0300

    18-11

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from "typeorm";
 import AppError from "../../../shared/errors/AppError";
 import User from "../typeorm/entities/User";
 import UserRepository from "../typeorm/repository/UserRepository";
+import {hash} from 'bcryptjs'
 
 // cria um tipo de dado
 interface IRequest {
@@ -19,6 +20,18 @@ class CreateUserService {
             throw new AppError(`Email já existente`, 400)
         }
         // email não existe
+        // senha do usuário precisa ser criptografada para inserção no banco de dados
+        let senhaCripto = await hash(password, 8) // 8 é quanto queremos temperar a criptografia
         
+        // cria o usuário em memória RAM
+        let novoUsuario = userRepository.create({
+            name, 
+            email,
+            password: senhaCripto
+        })
+        // salva no banco de dados - salva no disco
+        await userRepository.save(novoUsuario)
+        
+        return novoUsuario
     }
 }
