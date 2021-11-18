29437fa96700ebba0ce51c4e980bef4df6663569
dfpires@gmail.com <dfpires@gmail.com>
2021-11-18 20:38:40 -0300

    18-11

---
+++ src/modules/users/routes/routes.user.ts
@@ -0,0 +1,12 @@
+import {Router} from 'express'
+import UserController from '../controllers/UserController'
+
+const userRouter = Router()
+
+const userController = new UserController()
+
+userRouter.get('/', userController.index)
+
+userRouter.post('/', userController.create)
+
+export default userRouter
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import productRouter from "../../../modules/products/routes/routes.products";
+import userRouter from "../../../modules/users/routes/routes.user";
 
 const routes = Router()
 
 // acessando /product, poderemos executar get, post, put e delete 
 routes.use('/product', productRouter)
+
+routes.use('/user', userRouter)
 
 export default routes
