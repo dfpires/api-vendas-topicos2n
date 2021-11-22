08aeae3da31487b0ce1a0339fcb2d39f0005966d
dfpires@gmail.com <dfpires@gmail.com>
2021-11-22 20:02:48 -0300

    22-11

--- src/modules/products/routes/routes.products.ts
+++ src/modules/products/routes/routes.products.ts
@@ -1,15 +1,16 @@
 // vamos utilizar a classe Router do express
 import { Router } from "express";
+import isAuthenticated from "../../../shared/middleware/isAuthenticated";
 import ProductController from "../controllers/ProductController";
 
 let productRouter = Router()
 
 let productController = new ProductController()
 
-productRouter.get('/', productController.index)
-productRouter.get('/:id', productController.show)
-productRouter.post('/', productController.create)
-productRouter.put('/:id', productController.update)
-productRouter.delete('/:id', productController.delete)
+productRouter.get('/', isAuthenticated, productController.index)
+productRouter.get('/:id', isAuthenticated, productController.show)
+productRouter.post('/', isAuthenticated, productController.create)
+productRouter.put('/:id', isAuthenticated, productController.update)
+productRouter.delete('/:id', isAuthenticated, productController.delete)
 
 export default productRouter
--- src/shared/middleware/isAuthenticated.ts
+++ src/shared/middleware/isAuthenticated.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../errors/AppError";
+import {verify} from 'jsonwebtoken'
 
 export default function isAuthenticated(request: Request, response: Response, next: NextFunction): void {
     // recupera o token informado pelo usuário
@@ -9,7 +10,19 @@ export default function isAuthenticated(request: Request, response: Response, ne
         throw new AppError(`Token não está presente`, 400)
     }
     // frontend informou o token
-    
+    // Beared jkasjdkfjdsklfjkalsdjfkljasdklfjadskljfals
+    let [beared, token] = authHeaders.split(' ')
+    // verifica se o token é válido
+    try {
+        // devemos utilizar a chave secreta é privada
+        //jfalkjfklajdsklfjdsklafjasdklfj;kls
+        let tokenVerificado = verify(token, 'jfalkjfklajdsklfjdsklafjasdklfj;kls')
+        
+        return next() // passa pra frente, entra na rota
+    }
+    catch {
+        throw new AppError(`Token inválido`, 400)
+    }
 
     
 } 
