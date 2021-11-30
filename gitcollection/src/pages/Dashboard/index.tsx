f64f840f970e776ef78be7c60b9738da95e85807
dfpires@gmail.com <dfpires@gmail.com>
2021-11-29 22:09:19 -0300

    29-11

--- gitcollection/src/pages/Dashboard/index.tsx
+++ gitcollection/src/pages/Dashboard/index.tsx
@@ -5,12 +5,20 @@ import logo from '../../assets/logo.svg'
 
 export const Dashboard: React.FC = () => {
 
+    // criando um estado de variável e inicializo com vazio
+    const [novoRepositorio, setNovoRepositorio] = React.useState('')
+
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void{
+        // alterar o valor de novoRepositorio com o valor digitado pelo usuário
+        setNovoRepositorio(event.target.value)
+        
+    }
     return (
         <>
             <img src={logo} alt="Git Collection"/>
             <Title> Catálogo de repositórios do Github </Title>
             <Formulario>
-                <input placeholder="username/nome_repositorio"/>
+                <input placeholder="username/nome_repositorio" onChange={handleInputChange}/>
                 <button type="submit"> Buscar </button>
             </Formulario>
         </>
